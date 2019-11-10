488bdb5a9fb6bdb1979d0208933ee83f70c6f3fd
Colby Sieber <casieber.public@gmail.com>
2019-11-10 14:13:22 -0800

    Add skeleton of first package for firestore hooks

---
+++ packages/firestore/src/index.ts
@@ -0,0 +1,9 @@
+// WIP
+
+export function useCollection( collectionPath: string ) {
+    throw new Error( 'Not implemented.' );
+}
+
+export function useDocument( documentPath: string ) {
+    throw new Error( 'Not implemented.' );
+}
